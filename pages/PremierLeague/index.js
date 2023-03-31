9f038ed5fe999c7d72ae41c9b8e25a64b0ad390c
LucianoMariotti <luciano.mariotti99@gmail.com>
2023-03-30 21:39:35 -0300

    add retro page

--- pages/PremierLeague/index.js
+++ pages/PremierLeague/index.js
@@ -1,5 +1,52 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { motion, useScroll } from "framer-motion";
 import React from "react";
+import { useRef } from "react";
+import {
+  GundoganFigure,
+  SakaFigure,
+  SalahFigure,
+} from "../../components/Icons";
+import Layout from "../../components/Layout";
+import GridProductsList from "../../components/Shared/GridProductsList/GridProductsList";
 
 export default function PremierLeaguePage() {
-  return <div>PremierLeaguePage</div>;
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ container: ref });
+  return (
+    <motion.div
+      ref={ref}
+      style={{
+        position: "absolute",
+        height: "110vh",
+        width: "100%",
+        overflow: "scroll",
+      }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{
+        duration: 0.75,
+        delay: 0.5,
+        ease: "easeInOut",
+      }}
+    >
+      <Layout scrollYProgress={scrollYProgress}>
+        <Box layerStyle={"paddingX"}>
+          <Flex direction={"column"} align={"center"}>
+            <Text variant={"title"}>Premier League Shirts</Text>
+            <Text fontSize={{ base: "10px", tablet: "20px", desktop: "30px" }}>
+              Find all shirts of the league...
+            </Text>
+          </Flex>
+        </Box>
+        <Flex mt={"30px"} gap={10} justify={"center"} align={"center"}>
+          <GundoganFigure />
+          <SalahFigure />
+          <SakaFigure />
+        </Flex>
+        <GridProductsList title={"PL Shirts Collection"} />
+      </Layout>
+    </motion.div>
+  );
 }
