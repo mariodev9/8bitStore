4316286312b388f08713fa558a6ef0bb0ccbcc10
LucianoMariotti <luciano.mariotti99@gmail.com>
2023-03-30 21:32:56 -0300

    add Retro Page

---
+++ components/Shared/GridProductsList/GridProductsList.js
@@ -0,0 +1,25 @@
+import { Box, Grid, Text } from "@chakra-ui/react";
+import React from "react";
+import { ShirtsList } from "../../Shirts/ShirtsList";
+import ShirtGridItem from "../ShirtGridItem";
+
+export default function GridProductsList({ title }) {
+  return (
+    <Box layerStyle={"paddingX"} mt={"50px"}>
+      <Text variant={"sectionTitle"}>{title}</Text>
+      <Grid
+        py={"30px"}
+        templateColumns={{
+          base: "repeat(1, 1fr)",
+          tablet: "repeat(2, 1fr)",
+          desktop: "repeat(3, 1fr)",
+        }}
+        gap={6}
+      >
+        {ShirtsList.map((shirt) => (
+          <ShirtGridItem key={shirt.id} {...shirt} />
+        ))}
+      </Grid>
+    </Box>
+  );
+}
--- pages/Retro/index.js
+++ pages/Retro/index.js
@@ -1,17 +1,54 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperProducts from "../../components/Shared/SwiperProducts/SwiperProducts";
+import Layout from "../../components/Layout";
+import { useRef } from "react";
+import { useScroll } from "framer-motion";
+import { motion } from "framer-motion";
+
+import {
+  AdrianoFigure,
+  BaggioFigure,
+  TottiFigure,
+} from "../../components/Icons";
+import GridProductsList from "../../components/Shared/GridProductsList/GridProductsList";
 
 export default function RetroPage() {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ container: ref });
   return (
-    <div>
-      <Swiper>
-        <SwiperSlide>1</SwiperSlide>
-        <SwiperSlide>2</SwiperSlide>
-        <SwiperSlide>3</SwiperSlide>
-        <SwiperSlide>4</SwiperSlide>
-        <SwiperSlide>5</SwiperSlide>
-      </Swiper>
-    </div>
+    <motion.div
+      ref={ref}
+      style={{
+        position: "absolute",
+        height: "110vh",
+        width: "100%",
+        overflow: "scroll",
+      }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{
+        duration: 0.75,
+        delay: 0.5,
+        ease: "easeInOut",
+      }}
+    >
+      <Layout scrollYProgress={scrollYProgress}>
+        <Box layerStyle={"paddingX"}>
+          <Flex direction={"column"} align={"center"}>
+            <Text variant={"title"}>Retro Shirts</Text>
+            <Text fontSize={{ base: "10px", tablet: "20px", desktop: "30px" }}>
+              Find the jerseys worn by many world football legends.
+            </Text>
+            <Flex mt={"30px"} gap={10} align={"end"}>
+              <TottiFigure />
+              <AdrianoFigure />
+              <BaggioFigure />
+            </Flex>
+          </Flex>
+        </Box>
+        <GridProductsList title={"Retro Shirts Collection"} />
+      </Layout>
+    </motion.div>
   );
 }
